cc91fd1de3350b565d755a88f5eafd5a1b0d8a88
oleg_m23 <oleg_m23@bk.ru>
2022-08-23 12:08:35 +0300

    homework_6

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import './App.css';
-import { Route, Routes } from "react-router-dom";
-import Chats from "./pages/Chat";
-import Home from "./pages/Home";
-import NotFounds from "./pages/NotFound";
-import Profile from "./pages/Profile";
+import Count from './Count';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/chats" element={<Chats />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="*" element={<NotFounds />} />
-    </Routes>
+    <div>
+      <Count />
+    </div>
   );
 };
 
