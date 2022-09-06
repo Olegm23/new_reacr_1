921acfcec05343dcf460ccba51614edd909980a0
oleg_m23 <oleg_m23@bk.ru>
2022-09-06 12:53:54 +0300

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,7 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getPosts } from "../redux/postsReduser/postsReducer";
-
 function App() {
-  const posts = useSelector(state => state.posts);
-  const loading = useSelector(state => state.loading);
-  const error = useSelector(state => state.error);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts())
-  }, []);
-
-  const restart = () => {
-    dispatch(getPosts())
-  };
-
-  if (loading) {
-    return (
-      <div>
-        Идет загрузка....
-      </div>
-    )
-  }
-
-  if (error) {
-    return (
-      <div>
-        Произошла ошибка. <button onClick={restart} >Обновить</button>
-      </div>
-    )
-  }
 
   return (
     <div>
-      {posts.map((post) => {
-        return (
-          <div key={post.id}>
-            {post.title}
-          </div>
-        )
-      })}
     </div >
   );
 };
--- src/redux/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const GET_POSTS = 'GET_POSTS';
-export const LOAD_POSTS = 'LOAD_POSTS';
-export const ERROR_POSTS = 'ERROR_POSTS';
