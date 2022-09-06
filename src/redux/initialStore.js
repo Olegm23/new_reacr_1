0ca75cc1920c3f51cdd1f4159451ea5eb8ff8f61
oleg_m23 <oleg_m23@bk.ru>
2022-09-06 15:48:06 +0300

    change

--- src/redux/initialStore.js
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
-import { postsReducer } from "./postsReduser/postsReducer";
-
-export const store = createStore(postsReducer, applyMiddleware(thunk));
